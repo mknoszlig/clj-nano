035a84e8fcbbd9ac70c6ed73bbde3b914766119a
Maximilian Karasz <maximilian.karasz@googlemail.com>
2013-09-07 21:18:32 +0200

    small test adaptions.

--- src/clj_nano/perf/inproc_lat.clj
+++ src/clj_nano/perf/inproc_lat.clj
@@ -11,7 +11,7 @@
   (println (format "avg latency: %.3f [us]" (/ (double (- end-time begin-time))
                                                (* roundtrip-count 2 1000)))))
 
-(defn- mk-message [message-size]
+(defn mk-message [message-size]
   (byte-buffer (byte-array message-size (byte 111))))
 
 (defn mk-worker [message-size roundtrip-count done addr]
@@ -23,29 +23,32 @@
       (println (format "NANO PAIR socket connected to %s" addr))
       (println (format "NANO running %d iterations..." roundtrip-count))
       (dotimes [_ roundtrip-count]
-        (receive s buf  message-size)
+        (receive s buf message-size)
         (send s msg message-size))
       (close s)
       (.countDown done))))
 
-
 (defn run-benchmark [message-size roundtrip-count]
-  (let [addr "inproc://lat_test"
+  (let [addr "inproc://inproc_lat_test"
         s    (-> (socket :pair)
                  (bind addr))
         msg  (mk-message message-size)
         done (CountDownLatch. 1)
         buf  (ByteBuffer/allocateDirect message-size)]
     (println (format "NANO PAIR socket bound to %s" addr))
-    (.start (Thread. (mk-worker message-size roundtrip-count done addr)))
+    (.start (Thread. (mk-worker message-size
+                                roundtrip-count
+                                done
+                                addr)))
     (println (format "NANO running %d iterations.." roundtrip-count))
     (let [begin (System/nanoTime)]
       (dotimes [_ roundtrip-count]
         (send s msg message-size)
         (receive s buf message-size))
-      (.await done)
-      (close s)
-      (print-stats begin (System/nanoTime) message-size roundtrip-count))))
+      (let [end (System/nanoTime)]
+        (.await done)
+        (close s)
+        (print-stats begin end message-size roundtrip-count)))))
 
 (defn -main [msg-size iterations]
   (let [message-size (Integer/parseInt msg-size)
